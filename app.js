44162ec69bb6e755503d35a3ba72443ed4c21601
Alex Calia <alex.m.calia@outlook.com>
2020-11-19 16:04:44 -0500

    added time updater and shorthand timezone method

--- app.js
+++ app.js
@@ -6,15 +6,33 @@ clockApp.author = document.getElementById('author');
 clockApp.location = document.getElementById('location');
 clockApp.timezone= document.getElementById('timezone');
 clockApp.time = document.getElementById('time');
+clockApp.tzShort = document.getElementById('tzShort');
+clockApp.hour = document.getElementById('hour');
+clockApp.minutes = document.getElementById('minutes');
 
-// Time method
+// Get current time and create interval to update time
 clockApp.currentTime = () => {
   const date = new Date();
   const hour = date.getHours();
-  const min = date.getMinutes();
-  const sec = date.getSeconds()
-  var tzName = date.toLocaleString('en', {timeZoneName:'short'}).split(' ').pop();
-  console.log(tzName);
+  const minutes = date.getMinutes();
+  clockApp.hour.innerHTML = clockApp.updateTime(hour);
+  clockApp.minutes.innerHTML = clockApp.updateTime(minutes);
+  const t = setTimeout(() => clockApp.currentTime(), 60000);
+}
+
+// add a 0 to the time if it's a single digit
+clockApp.updateTime = (k) => {
+  if (k < 10) {
+    return "0" + k
+  } else {
+    return k
+  }
+}
+
+// Get the shorthand timezone
+clockApp.getTzShort = () => {
+  const date = new Date();
+  clockApp.tzShort.innerHTML = date.toLocaleString('en', {timeZoneName:'short'}).split(' ').pop();
 }
 
 // Method to get random quote
@@ -46,6 +64,7 @@ clockApp.init = () => {
   clockApp.eventListeners();
   clockApp.getQuote();
   clockApp.getGeoData();
+  clockApp.getTzShort();
   clockApp.currentTime();
 };
 
