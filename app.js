3ebab6bef35e17ac0b3182eee5123641cfb770ab
Alex Calia <alex.m.calia@outlook.com>
2020-11-18 22:24:09 -0500

    random quote system working, able to get new quotes with button click

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const clockApp = {}
 
+// Caching
+clockApp.quoteDOM = document.getElementById('quote');
+clockApp.authorDOM = document.getElementById('author');
+
+// Vartiables
+clockApp.quote = "";
+clockApp.author = "";
+
+clockApp.getQuote = async () => {
+  await fetch(`http://quotes.stormconsultancy.co.uk/random.json`)
+        .then(response => response.json())
+        .then(data => {
+          clockApp.quoteDOM.innerHTML = data.quote;
+          clockApp.authorDOM.innerHTML = `- ${data.author}`
+        });
+};
+
 clockApp.eventListeners = () => {
 
-}
+};
 
 clockApp.init = () => {
   clockApp.eventListeners();
-}
+  clockApp.getQuote();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  clockApp.init();
+});
