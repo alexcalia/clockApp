db2ac216e7829a0c516fd6d2ab7179400e04964d
Alex Calia <alex.m.calia@outlook.com>
2020-11-28 22:58:38 -0500

    created a function to set styles based on time of day to reduce repeated code

--- app.js
+++ app.js
@@ -19,35 +19,31 @@ clockApp.moreLessText = document.getElementById('moreLessText');
 clockApp.chevron = document.getElementById('chevron');
 clockApp.clockSection = document.getElementById('clockSection');
 
+clockApp.setTimeStyles = (greeting, sunMoon, background, backgroundSize, geoBack) => {
+    clockApp.greeting.innerHTML = greeting;
+    clockApp.sunMoon.className = sunMoon;
+    clockApp.body.style.background = background;
+    clockApp.body.style.backgroundSize = backgroundSize
+    clockApp.geoData.style.background = geoBack;
+  }
+
 // Get current time and create interval to update time
 clockApp.currentTime = () => {
   const date = new Date();
   const hour = date.getHours();
   const minutes = date.getMinutes();
   if (hour >= 12 && hour < 19) {
-    clockApp.greeting.innerHTML = "Afternoon";
-    clockApp.body.style.background = `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
-    url('./assets/dayBackground.jpg')`;
-    clockApp.body.style.backgroundSize = "100% 100%";
-    clockApp.geoData.style.background = "rgba(197, 197, 197, 0.9)";
+    clockApp.setTimeStyles("Afternoon", `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
+    url('./assets/dayBackground.jpg')`, "100% 100%", "rgba(197, 197, 197, 0.9)");
   } else if (hour >= 19) {
-    clockApp.greeting.innerHTML = "Evening";
-    clockApp.sunMoon.className = "fas fa-moon";
-    clockApp.body.style.background = `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
-    url('./assets/nightBackground.jpg')`;
-    clockApp.body.style.backgroundSize = "100% 100%";
+    clockApp.setTimeStyles("Evening", "fas fa-moon", `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
+    url('./assets/nightBackground.jpg')`, "100% 100%");
   } else if (hour >= 0 && hour < 4) {
-    clockApp.greeting.innerHTML = "Evening";
-    clockApp.body.style.background = `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
-    url('./assets/nightBackground.jpg')`;
-    clockApp.body.style.backgroundSize = "100% 100%";
+    clockApp.setTimeStyles("Evening", `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
+    url('./assets/nightBackground.jpg')`, "100% 100%");
   } else {
-    clockApp.greeting.innerHTML = "Morning";
-    clockApp.sunMoon.className = "fas fa-sun";
-    clockApp.body.style.background = `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
-    url('./assets/dayBackground.jpg')`;
-    clockApp.body.style.backgroundSize = "100% 100%";
-    clockApp.geoData.style.background = "rgba(197, 197, 197, 0.9)";
+    clockApp.setTimeStyles("Morning", "fas fa-sun", `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),
+    url('./assets/dayBackground.jpg')`, "100% 100%", "rgba(197, 197, 197, 0.9)")
   }
 
   clockApp.hour.innerHTML = clockApp.updateTime(hour);
